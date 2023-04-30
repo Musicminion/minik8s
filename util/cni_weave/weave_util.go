10a7b4c4ecae4ed752e4444a4f070b705ef89d25
breaking-wave <breaking-wave@sjtu.edu.cn>
2023-04-30 02:49:19 -0700

    feat: add weave util to enable attach

---
+++ util/cni_weave/weave_util.go
@@ -0,0 +1,17 @@
+package weaveutil
+
+import (
+	"miniK8s/pkg/k8log"
+	"os/exec"
+)
+
+func WeaveAttach(id, ip string) error {
+	if out, err := exec.Command("weave", "attach", ip, id).Output(); err != nil {
+		k8log.ErrorLog("Weave_util", "weave attch err: " + err.Error()) 
+		k8log.ErrorLog("Weave_util", "weave attch out: " + string(out))
+		return err
+	} else {
+		k8log.InfoLog("Weave_util", "weave attch out: " + string(out))
+		return nil
+	}
+}

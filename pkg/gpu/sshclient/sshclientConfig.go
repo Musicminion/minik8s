1255e61a188fe2ba268bf611e970e77fa798375f
Musicminion <zhangziqian@sjtu.edu.cn>
2023-05-17 08:03:51 -0700

    baisc gpu server

--- pkg/gpu/sshclient/sshclientConfig.go
+++ pkg/gpu/sshclient/sshclientConfig.go
@@ -13,9 +13,18 @@ const (
 	SSH_DATA_URL  = "data.hpc.sjtu.edu.cn"
 )
 
+const (
+	ChangeDirectoryCommand = "cd "
+	RemoveDirectoryCommand = "rm -rf "
+	MakeDirectoryCommand   = "mkdir -p "
+	ReadFileCommand        = "cat "
+	WriteFileCommand       = "echo "
+	AppendFileCommand      = "echo "
+	RemoveFileCommand      = "rm -rf "
+)
+
 func init() {
 	// 从环境变量中读取用户名和密码
 	SSHUserName = os.Getenv("GPU_SSH_USERNAME") // 从环境变量中读取用户名
 	SSHPassword = os.Getenv("GPU_SSH_PASSWORD") // 从环境变量中读取密码
-
 }

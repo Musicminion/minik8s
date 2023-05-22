0dd3da7630fd101344f6e7a7ed2d2ea4f3a84ff4
Musicminion <zhangziqian@sjtu.edu.cn>
2023-05-22 00:17:40 -0700

    feat: kubectl describe

--- pkg/kubectl/cmd/describe.go
+++ pkg/kubectl/cmd/describe.go
@@ -135,7 +135,31 @@ func describeNamespacePods(namespace string) {
 }
 
 func describeSpecificPod(namespace, name string) {
+	url := stringutil.Replace(config.PodSpecURL, config.URL_PARAM_NAMESPACE_PART, namespace)
+	url = stringutil.Replace(url, config.URL_PARAM_NAME_PART, name)
+	url = config.API_Server_URL_Prefix + url
 
+	pod := &apiObject.PodStore{}
+	code, err := netrequest.GetRequestByTarget(url, pod, "data")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if code != http.StatusOK {
+		fmt.Println("getSpecificPod: code:", code)
+		return
+	}
+
+	// 格式化输出JSON到控制台
+	indentedJSON, err := json.MarshalIndent(pod, "", "  ")
+	if err != nil {
+		fmt.Println("JSON marshaling failed:", err)
+		return
+	}
+
+	fmt.Println(string(indentedJSON))
 }
 
 // ==============================================
@@ -144,7 +168,91 @@ func describeSpecificPod(namespace, name string) {
 //
 // ==============================================
 func describeServiceHandler(cmd *cobra.Command, args []string) {
+	if len(args) == 1 {
+		// 尝试获取用户是否制定了namespace
+		namespace, _ := cmd.Flags().GetString("namespace")
 
+		if namespace == "" {
+			namespace = config.DefaultNamespace
+		}
+
+		// 获取default namespace下的所有service
+		describeNamespaceServices(namespace)
+	} else if len(args) == 2 {
+		// 获取namespace和serviceName
+		namespace, name, err := parseNameAndNamespace(args[1])
+
+		if err != nil {
+			fmt.Println("describeServiceHandler: parseNameAndNamespace error:", err)
+			return
+		}
+
+		if namespace == "" || name == "" {
+			fmt.Println("describeServiceHandler: namespace or name is empty")
+			fmt.Println("Use like: kubectl describe service [serviceNamespace]/[serviceName]")
+			return
+		}
+
+		// 获取特定的service
+		describeSpecificService(namespace, name)
+	}
+}
+
+func describeNamespaceServices(namespace string) {
+	url := stringutil.Replace(config.ServiceURL, config.URL_PARAM_NAMESPACE_PART, namespace)
+	url = config.API_Server_URL_Prefix + url
+
+	services := []apiObject.ServiceStore{}
+
+	code, err := netrequest.GetRequestByTarget(url, &services, "data")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if code != http.StatusOK {
+		fmt.Println("getNamespaceServices: code:", code)
+		return
+	}
+
+	// 格式化输出JSON到控制台
+	indentedJSON, err := json.MarshalIndent(services, "", "  ")
+
+	if err != nil {
+		fmt.Println("JSON marshaling failed:", err)
+		return
+	}
+
+	fmt.Println(string(indentedJSON))
+}
+
+func describeSpecificService(namespace, name string) {
+	url := stringutil.Replace(config.ServiceSpecURL, config.URL_PARAM_NAMESPACE_PART, namespace)
+	url = stringutil.Replace(url, config.URL_PARAM_NAME_PART, name)
+	url = config.API_Server_URL_Prefix + url
+
+	service := &apiObject.ServiceStore{}
+	code, err := netrequest.GetRequestByTarget(url, service, "data")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if code != http.StatusOK {
+		fmt.Println("getSpecificService: code:", code)
+		return
+	}
+
+	// 格式化输出JSON到控制台
+	indentedJSON, err := json.MarshalIndent(service, "", "  ")
+	if err != nil {
+		fmt.Println("JSON marshaling failed:", err)
+		return
+	}
+
+	fmt.Println(string(indentedJSON))
 }
 
 // ==============================================
@@ -153,7 +261,91 @@ func describeServiceHandler(cmd *cobra.Command, args []string) {
 //
 // ==============================================
 func describeJobHandler(cmd *cobra.Command, args []string) {
+	if len(args) == 1 {
+		// 尝试获取用户是否制定了namespace
+		namespace, _ := cmd.Flags().GetString("namespace")
 
+		if namespace == "" {
+			namespace = config.DefaultNamespace
+		}
+
+		// 获取default namespace下的所有job
+		describeNamespaceJobs(namespace)
+	} else if len(args) == 2 {
+		// 获取namespace和jobName
+		namespace, name, err := parseNameAndNamespace(args[1])
+
+		if err != nil {
+			fmt.Println("describeJobHandler: parseNameAndNamespace error:", err)
+			return
+		}
+
+		if namespace == "" || name == "" {
+			fmt.Println("describeJobHandler: namespace or name is empty")
+			fmt.Println("Use like: kubectl describe job [jobNamespace]/[jobName]")
+			return
+		}
+
+		// 获取特定的job
+		describeSpecificJob(namespace, name)
+	}
+}
+
+func describeNamespaceJobs(namespace string) {
+	url := stringutil.Replace(config.JobsURL, config.URL_PARAM_NAMESPACE_PART, namespace)
+	url = config.API_Server_URL_Prefix + url
+
+	jobs := []apiObject.JobStore{}
+
+	code, err := netrequest.GetRequestByTarget(url, &jobs, "data")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if code != http.StatusOK {
+		fmt.Println("getNamespaceJobs: code:", code)
+		return
+	}
+
+	// 格式化输出JSON到控制台
+	indentedJSON, err := json.MarshalIndent(jobs, "", "  ")
+
+	if err != nil {
+		fmt.Println("JSON marshaling failed:", err)
+		return
+	}
+
+	fmt.Println(string(indentedJSON))
+}
+
+func describeSpecificJob(namespace, name string) {
+	url := stringutil.Replace(config.JobSpecURL, config.URL_PARAM_NAMESPACE_PART, namespace)
+	url = stringutil.Replace(url, config.URL_PARAM_NAME_PART, name)
+	url = config.API_Server_URL_Prefix + url
+
+	job := &apiObject.JobStore{}
+	code, err := netrequest.GetRequestByTarget(url, job, "data")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if code != http.StatusOK {
+		fmt.Println("getSpecificJob: code:", code)
+		return
+	}
+
+	// 格式化输出JSON到控制台
+	indentedJSON, err := json.MarshalIndent(job, "", "  ")
+	if err != nil {
+		fmt.Println("JSON marshaling failed:", err)
+		return
+	}
+
+	fmt.Println(string(indentedJSON))
 }
 
 // ==============================================
@@ -162,5 +354,77 @@ func describeJobHandler(cmd *cobra.Command, args []string) {
 //
 // ==============================================
 func describeReplicasetHandler(cmd *cobra.Command, args []string) {
+	if len(args) == 1 {
+		// 尝试获取用户是否制定了namespace
+		namespace, _ := cmd.Flags().GetString("namespace")
+
+		if namespace == "" {
+			namespace = config.DefaultNamespace
+		}
+
+		// 获取default namespace下的所有replicaset
+		describeNamespaceReplicasets(namespace)
+	} else if len(args) == 2 {
+		// 获取namespace和replicasetName
+		namespace, name, err := parseNameAndNamespace(args[1])
+
+		if err != nil {
+			fmt.Println("describeReplicasetHandler: parseNameAndNamespace error:", err)
+			return
+		}
+
+		if namespace == "" || name == "" {
+			fmt.Println("describeReplicasetHandler: namespace or name is empty")
+			fmt.Println("Use like: kubectl describe replicaset [replicasetNamespace]/[replicasetName]")
+			return
+		}
+
+		// 获取特定的replicaset
+		describeSpecificReplicaset(namespace, name)
+	} else {
+		fmt.Println("describeReplicasetHandler: args error")
+
+	}
+}
+
+func describeNamespaceReplicasets(namespace string) {
+	url := stringutil.Replace(config.ReplicaSetsURL, config.URL_PARAM_NAMESPACE_PART, namespace)
+
+	url = config.API_Server_URL_Prefix + url
+
+	replicasets := []apiObject.ReplicaSetStore{}
+
+	code, err := netrequest.GetRequestByTarget(url, &replicasets, "data")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if code != http.StatusOK {
+		fmt.Println("getNamespaceReplicasets: code:", code)
+		return
+	}
+
+}
+
+func describeSpecificReplicaset(namespace, name string) {
+	url := stringutil.Replace(config.ReplicaSetSpecURL, config.URL_PARAM_NAMESPACE_PART, namespace)
+	url = stringutil.Replace(url, config.URL_PARAM_NAME_PART, name)
+	url = config.API_Server_URL_Prefix + url
+
+	replicaset := &apiObject.ReplicaSetStore{}
+
+	code, err := netrequest.GetRequestByTarget(url, replicaset, "data")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if code != http.StatusOK {
+		fmt.Println("getSpecificReplicaset: code:", code)
+		return
+	}
 
 }
